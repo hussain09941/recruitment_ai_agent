5f727f304c0832ab11fb92efc46a167bda3a6029
Jabir Hussain <hussain.csd@gmail.com>
2026-01-25 15:58:52 +0530

    use env-based SECRET_KEY

--- recruitment_ai/settings.py
+++ recruitment_ai/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-os.environ.get("SECRET_KEy","unsafe-development-key")
+os.environ.get("SECRET_KEY","unsafe-development-key")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
