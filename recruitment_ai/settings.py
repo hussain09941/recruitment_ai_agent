0d09b02791ba248112779651723fddf9b4909713
Jabir Hussain <hussain.csd@gmail.com>
2026-01-25 16:05:47 +0530

    Ready for deployment

--- recruitment_ai/settings.py
+++ recruitment_ai/settings.py
@@ -11,8 +11,22 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+
 import os
-os.environ.get("SECRET_KEY","unsafe-development-key")
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-change-this-only-for-local"
+)
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    ".onrender.com,localhost,127.0.0.1"
+).split(",")
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
